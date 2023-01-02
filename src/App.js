a453da7d4b54902de376c85dfc75d4fcaeb9aa83
Giovanne Mendonça <giovannemendonca@outlook.com>
2023-01-02 14:33:07 -0300

    Add layout

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Board from "./components/Board";
+import Header from "./components/Header";
+import GlobalStyle from "./styles/global";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Board /> 
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Label } from "./styles";
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159C1" />
+      </header>
+      <p>Fazer migração completa do servidor</p>
+      <img src="https://avatars.githubusercontent.com/u/96134018?" alt="" />
+    </Container>
+  );
+}
