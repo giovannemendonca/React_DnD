53f178836394dbc7be25cf0a67d52020a2bffa8b
Giovanne Mendonça <giovannemendonca@outlook.com>
2023-01-02 23:28:58 -0300

    finalizando projeto

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import produce from "immer";
 import { Container } from "./styles";
+import BoardContext from "./context";
 import List from "../List";
 import { loadLists } from "../../services/api";
 
-const lists = loadLists();
+const data = loadLists();
 
 export default function Board() {
+  const [lists, setList] = useState(data);
+
+  function move(fromList, toList, from, to) {
+    setList(
+      produce(lists, (draft) => {
+        const dragged = draft[fromList].cards[from];
+        draft[fromList].cards.splice(from, 1)
+        draft[toList].cards.splice(to, 0, dragged)
+      })
+    );
+  }
+
   return (
-    <Container>
-      {lists.map((list) => (
-        <List key={list.title} data={list} />
-      ))}
-    </Container>
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) => (
+          <List key={list.title} data={list} index={index} />
+        ))}
+      </Container>
+    </BoardContext.Provider>
   );
 }
